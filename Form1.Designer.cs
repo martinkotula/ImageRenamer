21d3f31024b2eb33ee4e331784bdfde4b2c794b2
Martin Kotula <kotula.mar@gmail.com>
2016-02-07 22:59:33 +0000

    Solution files commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,361 @@
+﻿namespace PictureRenamer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.images = new System.Windows.Forms.ImageList(this.components);
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.flowLayoutPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.MoveToButton = new System.Windows.Forms.Button();
+            this.MoveDownButton = new System.Windows.Forms.Button();
+            this.MoveUpButton = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.renameButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.outputDirectoryTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.fileNameTextBox = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.progressStatusBar = new System.Windows.Forms.StatusStrip();
+            this.toolStripProgressBar = new System.Windows.Forms.ToolStripProgressBar();
+            this.toolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.outputFolderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.flowLayoutPanel.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.progressStatusBar.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // images
+            // 
+            this.images.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit;
+            this.images.ImageSize = new System.Drawing.Size(256, 256);
+            this.images.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 24);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.flowLayoutPanel);
+            this.splitContainer1.Panel1.Enabled = false;
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.listView1);
+            this.splitContainer1.Size = new System.Drawing.Size(831, 499);
+            this.splitContainer1.SplitterDistance = 184;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // flowLayoutPanel
+            // 
+            this.flowLayoutPanel.Controls.Add(this.groupBox1);
+            this.flowLayoutPanel.Controls.Add(this.groupBox2);
+            this.flowLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel.Name = "flowLayoutPanel";
+            this.flowLayoutPanel.Size = new System.Drawing.Size(184, 499);
+            this.flowLayoutPanel.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Controls.Add(this.MoveToButton);
+            this.groupBox1.Controls.Add(this.MoveDownButton);
+            this.groupBox1.Controls.Add(this.MoveUpButton);
+            this.groupBox1.Location = new System.Drawing.Point(3, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(178, 212);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Order";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(12, 161);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(15);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(50, 20);
+            this.numericUpDown1.TabIndex = 0;
+            // 
+            // MoveToButton
+            // 
+            this.MoveToButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.MoveToButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.MoveToButton.Location = new System.Drawing.Point(67, 143);
+            this.MoveToButton.Name = "MoveToButton";
+            this.MoveToButton.Size = new System.Drawing.Size(104, 52);
+            this.MoveToButton.TabIndex = 2;
+            this.MoveToButton.Text = "Move &To";
+            this.MoveToButton.UseVisualStyleBackColor = true;
+            this.MoveToButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // MoveDownButton
+            // 
+            this.MoveDownButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.MoveDownButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.MoveDownButton.Location = new System.Drawing.Point(12, 77);
+            this.MoveDownButton.Name = "MoveDownButton";
+            this.MoveDownButton.Size = new System.Drawing.Size(159, 52);
+            this.MoveDownButton.TabIndex = 1;
+            this.MoveDownButton.Text = "Move &Down";
+            this.MoveDownButton.UseVisualStyleBackColor = true;
+            this.MoveDownButton.Click += new System.EventHandler(this.MoveDownButton_Click);
+            // 
+            // MoveUpButton
+            // 
+            this.MoveUpButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.MoveUpButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.MoveUpButton.Location = new System.Drawing.Point(12, 19);
+            this.MoveUpButton.Name = "MoveUpButton";
+            this.MoveUpButton.Size = new System.Drawing.Size(159, 52);
+            this.MoveUpButton.TabIndex = 0;
+            this.MoveUpButton.Text = "Move &Up";
+            this.MoveUpButton.UseVisualStyleBackColor = true;
+            this.MoveUpButton.Click += new System.EventHandler(this.MoveUpButton_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.renameButton);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.outputDirectoryTextBox);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.fileNameTextBox);
+            this.groupBox2.Location = new System.Drawing.Point(3, 221);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(177, 184);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Rename";
+            // 
+            // renameButton
+            // 
+            this.renameButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.renameButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.renameButton.Location = new System.Drawing.Point(12, 113);
+            this.renameButton.Name = "renameButton";
+            this.renameButton.Size = new System.Drawing.Size(159, 52);
+            this.renameButton.TabIndex = 3;
+            this.renameButton.Text = "Rename";
+            this.renameButton.UseVisualStyleBackColor = true;
+            this.renameButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(145, 84);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(26, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "O";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // outputDirectoryTextBox
+            // 
+            this.outputDirectoryTextBox.Location = new System.Drawing.Point(12, 87);
+            this.outputDirectoryTextBox.Name = "outputDirectoryTextBox";
+            this.outputDirectoryTextBox.Size = new System.Drawing.Size(127, 20);
+            this.outputDirectoryTextBox.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Output directory";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "File Name";
+            // 
+            // fileNameTextBox
+            // 
+            this.fileNameTextBox.Location = new System.Drawing.Point(12, 43);
+            this.fileNameTextBox.Name = "fileNameTextBox";
+            this.fileNameTextBox.Size = new System.Drawing.Size(159, 20);
+            this.fileNameTextBox.TabIndex = 0;
+            // 
+            // listView1
+            // 
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.HideSelection = false;
+            this.listView1.LargeImageList = this.images;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(643, 499);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // progressStatusBar
+            // 
+            this.progressStatusBar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripProgressBar,
+            this.toolStripStatusLabel});
+            this.progressStatusBar.Location = new System.Drawing.Point(0, 501);
+            this.progressStatusBar.Name = "progressStatusBar";
+            this.progressStatusBar.Size = new System.Drawing.Size(831, 22);
+            this.progressStatusBar.TabIndex = 1;
+            this.progressStatusBar.Text = "statusStrip1";
+            this.progressStatusBar.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.progressStatusBar_ItemClicked);
+            // 
+            // toolStripProgressBar
+            // 
+            this.toolStripProgressBar.Name = "toolStripProgressBar";
+            this.toolStripProgressBar.Size = new System.Drawing.Size(100, 16);
+            // 
+            // toolStripStatusLabel
+            // 
+            this.toolStripStatusLabel.Name = "toolStripStatusLabel";
+            this.toolStripStatusLabel.Size = new System.Drawing.Size(83, 17);
+            this.toolStripStatusLabel.Text = "Load directory";
+            this.toolStripStatusLabel.Click += new System.EventHandler(this.toolStripStatusLabel_Click);
+            // 
+            // outputFolderBrowserDialog
+            // 
+            this.outputFolderBrowserDialog.RootFolder = System.Environment.SpecialFolder.DesktopDirectory;
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.Filter = "JPG|*.jpg|All files|*.*";
+            this.openFileDialog.Multiselect = true;
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "&Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(831, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(831, 523);
+            this.Controls.Add(this.progressStatusBar);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Renamer";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.flowLayoutPanel.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.progressStatusBar.ResumeLayout(false);
+            this.progressStatusBar.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ImageList images;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel;
+        private System.Windows.Forms.Button MoveUpButton;
+        private System.Windows.Forms.Button MoveDownButton;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button MoveToButton;
+        private System.Windows.Forms.StatusStrip progressStatusBar;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button renameButton;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox outputDirectoryTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox fileNameTextBox;
+        private System.Windows.Forms.FolderBrowserDialog outputFolderBrowserDialog;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace PictureRenamer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            if (Directory.Exists(@"D:\foto"))
+            {
+                openFileDialog.InitialDirectory = @"D:\foto";
+            }
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var result = openFileDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                LoadImages(openFileDialog.FileNames);
+                outputDirectoryTextBox.Text = Path.GetDirectoryName(openFileDialog.FileName);
+                outputFolderBrowserDialog.SelectedPath = Path.GetDirectoryName(openFileDialog.FileName);
+            }
+        }
+
+        private void LoadImages(string[] files)
+        {
+            toolStripStatusLabel.Text = string.Format("Loading {0} images", toolStripProgressBar.Maximum);
+            listView1.BeginUpdate();
+            listView1.Items.Clear();
+            var imagesCollection = this.images.Images;
+            foreach (Image img in imagesCollection)
+            {
+                img.Dispose();
+            }
+
+            imagesCollection.Clear();
+            var i = 0;
+            
+            StartBatch(files.Count());
+            
+            foreach (var file in files)
+            {
+                using (var img = Image.FromFile(file))
+                {
+                    var sizedImage = new Bitmap(img, 256, 256);
+                    imagesCollection.Add(sizedImage);
+                    listView1.Items.Add(new ListViewItem(new[] { i.ToString(), file }, i++));
+
+                    toolStripProgressBar.Value++;
+                }
+            }
+
+            foreach (ListViewItem item in listView1.Items)
+            {
+                item.Text = string.Format("[{0}] {1}", item.Index+1, item.SubItems[1].Text);
+            }
+            
+            listView1.EndUpdate();
+
+            numericUpDown1.Minimum = 1;
+            numericUpDown1.Maximum = listView1.Items.Count;
+
+            BatchFinished();
+            toolStripStatusLabel.Text = string.Format("{0} images", toolStripProgressBar.Maximum);
+        }
+
+        private void StartBatch(int filesCount)
+        {
+            toolStripProgressBar.Visible = true;
+            toolStripProgressBar.Value = 0;
+            toolStripProgressBar.Maximum = filesCount;
+            toolStripStatusLabel.Visible = true;
+            splitContainer1.Panel1.Enabled = false;
+        }
+
+        private void BatchFinished()
+        {
+            splitContainer1.Panel1.Enabled = true;
+            toolStripProgressBar.Visible = false;
+            
+        }
+
+        private void MoveUpButton_Click(object sender, EventArgs e)
+        {
+            
+            if (listView1.SelectedItems.Count > 0)
+            {
+                var item = listView1.SelectedItems[0];
+                if (item.Index > 0)
+                {
+                    MoveItem(item, item.Index - 1);
+                }
+            }
+        }
+
+        private void MoveItem(ListViewItem item, int newIndex)
+        {
+            listView1.View = View.List;
+            var items = listView1.Items;
+            var movedItem = items[newIndex];
+            items.Remove(item);
+            items.Insert(newIndex, item);
+            listView1.View = View.LargeIcon;
+
+            UpdateItemText(item);
+            UpdateItemText(movedItem);
+
+            listView1.EnsureVisible(newIndex);
+        }
+
+        private void UpdateItemText(ListViewItem item)
+        {
+            item.Text = string.Format("[{0}] {1}", item.Index + 1, item.SubItems[1].Text);
+        }
+
+
+        private void MoveDownButton_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                var item = listView1.SelectedItems[0];
+                if (item.Index < listView1.Items.Count)
+                {
+                    MoveItem(item, item.Index + 1);
+                }
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                MoveUpButton.Enabled = listView1.SelectedItems[0].Index > 0;
+                MoveDownButton.Enabled = listView1.SelectedItems[0].Index < listView1.Items.Count-1;
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                var item = listView1.SelectedItems[0];
+                MoveItem(item, (int)numericUpDown1.Value - 1);
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            var result = outputFolderBrowserDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                outputDirectoryTextBox.Text = outputFolderBrowserDialog.SelectedPath;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var outputDirectory = outputDirectoryTextBox.Text;
+
+            if (!Directory.Exists(outputDirectory))
+            {
+                MessageBox.Show("Output directory does not exist! Please pick an existing output directory", "Output directory not found", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            var fileName = fileNameTextBox.Text;
+            if (string.IsNullOrEmpty(fileName))
+            {
+                MessageBox.Show("File name not set! Please specify new file name", "File name not set", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            StartBatch(listView1.Items.Count);
+            toolStripStatusLabel.Text = string.Format("Renaming {0} images", toolStripProgressBar.Maximum);
+            var fileNumber = 1;
+            foreach (ListViewItem file in listView1.Items)
+            {
+                var extension = Path.GetExtension(file.SubItems[1].Text);
+                File.Copy(file.SubItems[1].Text, Path.Combine(outputDirectory, string.Format("{0}_{1}{2}", fileName, fileNumber, extension)));
+                fileNumber++;
+                toolStripProgressBar.Value++;
+            }
+            Process.Start(outputDirectory);
+            BatchFinished();
+        }
+
+        private void progressStatusBar_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void toolStripStatusLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
